da350da08cc3a5206f95b1ef71c9235b80c90414
sg88995 <sofiacarla.garcia@supervielle.com.ar>
2025-03-03 21:16:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mundo-prendarios',
+  webDir: 'www'
+};
+
+export default config;
